f2c3ba9f9c6982304f42b410ed2b1529bfca4257
허재 <jea1257@naver.com>
2024-11-11 18:04:58 +0900

    11/11 학원 내용

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요");
+        // int 나이;
+        // 나이 = 20;
+        int 나이 = 20;
+        나이 = 나이 +1;
+        나이 = 나이 +1;
+        나이 = 나이 +1;
+        나이 = 나이 +1;
+
+        System.out.println("나이 = " + 나이);
+
+
+        int count = 5;
+        count = count -1;
+        count = count -1;
+        count = count -1;
+        count = count -1;
+
+        System.out.println("count = " + count);
+
+        int a = 3;
+        int b = 6;
+        int c = 9;
+
+        int temp = a;
+        a = c; // a = 9
+        c = b; // c = 6
+        b = temp; // b = 3
+
+
+        System.out.println("a = " + a); //9
+        System.out.println("b = " + b); //3
+        System.out.println("c = " + c); //6
+
+        int n = 20;
+        n = -20;
+
+        System.out.println("n = " + n); //n = -20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
