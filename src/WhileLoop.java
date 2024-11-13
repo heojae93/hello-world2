3196b776d55a9649d987b01a49ebc5751611127e
허재 <jea1257@naver.com>
2024-11-13 18:18:36 +0900

    11/13 학원 내용

---
+++ src/WhileLoop.java
@@ -0,0 +1,19 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int n = 1;
+        for (int i = 0; i < 9; i++) {
+            System.out.println(n);
+            n = n +1;
+        }
+
+        for (int i = 2; i <= 9; i++) {
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+}
